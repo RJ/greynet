975128a7609d525be9a36af16f71996eb4c7c08a
RJ <rj@metabrew.com>
2009-07-09 21:13:23 +0100

    hack to detect playdar without relying on disco, for gtalk :(

--- src/jbot.cpp
+++ src/jbot.cpp
@@ -320,7 +320,8 @@ void
 jbot::handleDiscoInfo( const JID& from, const Disco::Info& info, int context)
 {
     printf("///////// handleDiscoInfo!\n");
-    if ( info.hasFeature("playdar:resolver") )
+    if ( info.hasFeature("playdar:resolver") 
+         || from.full().find( "playdar") != string::npos ) // HACK, gtalk filters unrecognised disco features, apparently :|
     {
         printf( "Found contact with playdar capabilities! '%s'\n", from.full().c_str() );
         bool found = false;
